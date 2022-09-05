d4ceeae4a0df224c664ad00598714b4d47c3836b
lshn1007 <lshn1007@hanyang.ac.kr>
2022-09-05 22:01:08 +0900

    feat: boiler plate 추가

---
+++ src/main/java/com/shwanlog/ShwanlogApplication.java
@@ -0,0 +1,13 @@
+package com.shwanlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShwanlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShwanlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shwanlog/ShwanlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shwanlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShwanlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
