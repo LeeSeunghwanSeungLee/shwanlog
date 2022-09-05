f8c989c5e83f421364c06321649def43a641fc3b
lshn1007 <lshn1007@hanyang.ac.kr>
2022-09-05 22:13:38 +0900

    feat: POSTS 컨트롤러 추가 및 테스트 작성

---
+++ src/test/java/com/shwanlog/controller/PostControllerTest.java
@@ -0,0 +1,25 @@
+package com.shwanlog.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class PostControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 요청시 Hello World 출력")
+    void test_1() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello World"));
+    }
+}
