f28ec4a36b0d5151e67dbfb69cc0594d48eeab7d
lshn1007 <lshn1007@hanyang.ac.kr>
2022-09-10 23:31:51 +0900

    feat: PostCreateDto 빌더패턴 적용 및 테스트코드 변경

--- src/test/java/com/shwanlog/controller/PostControllerTest.java
+++ src/test/java/com/shwanlog/controller/PostControllerTest.java
@@ -1,7 +1,9 @@
 package com.shwanlog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shwanlog.domain.Post;
 import com.shwanlog.repository.PostRepository;
+import com.shwanlog.request.PostCreateDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -29,6 +31,8 @@ class PostControllerTest {
     @Autowired
     private PostRepository postRepository;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @BeforeEach
     void reset() {
         this.postRepository.deleteAll();
@@ -37,8 +41,14 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 Hello World 출력")
     void test_1() throws Exception {
+        PostCreateDto postCreateDto = new PostCreateDto().builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+        String inputValue = this.objectMapper.writeValueAsString(postCreateDto);
+
         mockMvc.perform(post("/posts")
-                .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
+                .content(inputValue)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World"))
@@ -48,8 +58,13 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 타이틀이 없는 경우.")
     void test_2() throws Exception {
+        PostCreateDto postCreateDto = new PostCreateDto().builder()
+                .content("내용입니다.")
+                .build();
+        String inputValue = this.objectMapper.writeValueAsString(postCreateDto);
+
         mockMvc.perform(post("/posts")
-                .content("{\"title\": \"\", \"content\": \"내용입니다.\"}")
+                .content(inputValue)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
                 .andDo(print());
@@ -58,8 +73,13 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 컨텐츠가 없는 경우.")
     void test_3() throws Exception {
+        PostCreateDto postCreateDto = new PostCreateDto().builder()
+                .title("제목입니다.")
+                .build();
+        String inputValue = this.objectMapper.writeValueAsString(postCreateDto);
+
         mockMvc.perform(post("/posts")
-                .content("{\"title\": \"제목입니다.\", \"content\": \"\"}")
+                .content(inputValue)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
                 .andDo(print());
@@ -68,8 +88,14 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 db에 데이터가 저장된다.")
     void test_4() throws Exception {
+        PostCreateDto postCreateDto = new PostCreateDto().builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+        String inputValue = this.objectMapper.writeValueAsString(postCreateDto);
+
         mockMvc.perform(post("/posts")
-                        .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
+                        .content(inputValue)
                         .contentType("application/json"))
                 .andExpect(status().isOk())
                 .andDo(print());
